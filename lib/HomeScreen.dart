6899e4791967234a3a08668570863efac0fd6210
Chandrasekar Kuppusamy <takeoffandroid@gmail.com>
2019-04-16 22:36:11 +0530

    Restricted the click event for a grid

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:tictactoe/ComputerWidget.dart';
@@ -6,6 +8,7 @@ import 'package:tictactoe/GameRules.dart';
 import 'package:tictactoe/GameState.dart';
 import 'package:tictactoe/PlayerWidget.dart';
 import 'package:tictactoe/WinWidget.dart';
+import 'package:connectivity/connectivity.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -16,6 +19,8 @@ class _HomePageState extends State<HomeScreen> {
   GameRules gameRules;
 
   bool isChecked = false;
+
+  bool isDisableClick = false;
 
   @override
   void initState() {
@@ -59,7 +64,7 @@ class _HomePageState extends State<HomeScreen> {
           ),
           body: Center(
             child: GridView.builder(
-              shrinkWrap: true,
+                shrinkWrap: true,
                 itemCount: gameRules.getCount(),
                 padding: EdgeInsets.all(16),
                 gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(
@@ -67,19 +72,26 @@ class _HomePageState extends State<HomeScreen> {
                 itemBuilder: (BuildContext context, int index) {
                   GameState gameState = gameRules.getGameState(index);
                   String value = gameState.getValue;
-                  getGridRow(value);
                   return new GestureDetector(
                     child: getGridRow(value),
                     onTap: () {
-                      if (gameRules.isNewMove(index)) {
-                        setState(() {
-                          gameRules.updateState(
-                              index,
-                              GameState(
-                                  position: gameState.position,
-                                  value: GameConstants.PLAYER));
+                      if (!isDisableClick && gameRules.isNewMove(index)) {
+                        isNetworkAvailable().then((isThere) {
+                          if (isThere) {
+                            setState(() {
+                              gameRules.updateState(
+                                  index,
+                                  GameState(
+                                      position: gameState.position,
+                                      value: GameConstants.PLAYER));
+                              isDisableClick = true;
+                            });
+                            postData(index, gameState);
+                          } else {
+                            debugPrint(
+                                'No moves can be done if there is no internet connection');
+                          }
                         });
-                          postData(index, gameState);
                       }
                     },
                   );
@@ -114,11 +126,19 @@ class _HomePageState extends State<HomeScreen> {
   }
 
   Future<void> postData(int index, GameState gameState) async {
-    debugPrint("Printing request "+ gameRules.getBoardJSON());
-    http.Response res = await http.post("https://fathomless-savannah-49582.herokuapp.com/play", body: gameRules.getBoardJSON()); // post api call
-    debugPrint("Response body "+ res.body.toString());
+    debugPrint("Printing request " + gameRules.getBoardJSON());
+    http.Response res = await http.post(
+        "https://fathomless-savannah-49582.herokuapp.com/play",
+        body: gameRules.getBoardJSON());
+    debugPrint("Response body " + res.body.toString());
     setState(() {
+      isDisableClick = false;
       gameRules.updateStates(gameRules.getGameStatesFromResponse(res.body));
     });
+  }
+
+  Future<bool> isNetworkAvailable() async {
+    var connectivityResult = await (Connectivity().checkConnectivity());
+    return connectivityResult != ConnectivityResult.none;
   }
 }
